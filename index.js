44cb3927b361907022906a94c5d2733db92858d1
Oleg Kulyk <okami4kak@gmail.com>
2017-03-23 15:40:07 +0200

    Initial module structure

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-var exec = require('child_process').execSync;
+var exec = require('child_process').exec;
+var os = require('os');
+var path = require('path');
 
-function checkWin(path) {
+var win32 = 'win32';
+var darwin = 'darwin';
 
+function checkWin(path, callback) {
+    if (!path.existsSync(path)) {
+        callback(null, 'No such path');
+    }
+
+    if (os.platform() !== win32) {
+        callback(null, 'Can not check Win sign on current platform');
+    }
 }
 
-function checkMac(path) {
+function checkMac(path, callback) {
+    if (!path.existsSync(path)) {
+        callback(null, 'No such path');
+    }
 
+    if (os.platform() !== darwin) {
+        callback(null, 'Can not check macOS sign on current platform');
+    }
+
+    try {
+        var execProcess = exec('codesign --verify "' + path + '"');
+        execProcess.on('exit', function (code) {
+            if (code === 0) {
+                callback(true);
+            } else {
+                callback(false);
+            }
+        });
+    } catch (error) {
+        callback(null, 'Unable to verify file sign ' + error);
+    }
 }
 
 module.exports.checkWin = checkWin;
