52868ff77d608a22ab129d88c22b1fd2673d2a93
Oleg Kulyk <okami4kak@gmail.com>
2017-03-23 14:01:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var exec = require('child_process').execSync;
+
+function checkWin(path) {
+
+}
+
+function checkMac(path) {
+
+}
+
+module.exports.checkWin = checkWin;
+module.exports.checkMac = checkMac;
